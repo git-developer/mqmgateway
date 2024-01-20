8ad954f9599ffe26ce5b1c3026670b4e45f21350
Christian <git-developer@users.noreply.github.com>
2024-01-20 10:55:06 +0100

    wip: tweak test timeout

--- unittests/single_register_tests.cpp
+++ unittests/single_register_tests.cpp
@@ -78,7 +78,7 @@ mqtt:
         server.disconnectModbusSlave("tcptest", 1);
 
         //max 4 sec for three register read attempts
-        server.waitForPublish("test_switch/availability", std::chrono::seconds(4));
+        server.waitForPublish("test_switch/availability", std::chrono::seconds(5));
         REQUIRE(server.mqttValue("test_switch/availability") == "0");
         server.stop();
     }
